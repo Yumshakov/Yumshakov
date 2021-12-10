d6514dca5ed893b6d89aa875b8b397b9412037e2
Yumshakov <chyrilovo92@mail.ru>
2021-12-10 16:35:56 +0500

    Tested Seleinium

--- learning/learn_Selenium/selenium_env/lesson_1_1.py
+++ learning/learn_Selenium/selenium_env/lesson_1_1.py
@@ -1,10 +1,12 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 # инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
 driver = webdriver.Chrome()
 # команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(5)
+time.sleep(1)
 # Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
 driver.get("https://stepik.org/lesson/25969/step/12")
 time.sleep(5)
