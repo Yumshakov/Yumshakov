4484d8ac5fbba971ab3b3f6de61d91e71b5a474e
Yumshakov <chyrilovo92@mail.ru>
2021-12-21 17:53:58 +0500

    learned get atribut and implicitly

--- learning/learn_Selenium/selenium_env/lesson_2_2_6.py
+++ learning/learn_Selenium/selenium_env/lesson_2_2_6.py
@@ -1,30 +1,37 @@
 from selenium import webdriver
-import time
-import math
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
+import time
 
 
 def calc(x):
 
   return str(math.log(abs(12*math.sin(int(x)))))
 
+
 def main_functions():
 
-    browser.get(url)
-    browser.find_element(By.TAG_NAME, 'button').click()
-    browser.switch_to.window(browser.window_handles[1])
+    WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, 'price'), '100'))
+    browser.find_element(By.ID, "book").click()
     find_attribute = browser.find_element(By.ID, 'input_value')
     y = calc(find_attribute.text)
     input_result = browser.find_element(By.ID, "answer")
     input_result.send_keys(y)
-    btn_submit = browser.find_element(By.CSS_SELECTOR, "button")
+    btn_submit = browser.find_element(By.ID, "solve")
     btn_submit.click()
-    browser.im
+
+
 if __name__ == '__main__':
     try:
-        url = "http://suninjuly.github.io/redirect_accept.html"
+        url = 'http://suninjuly.github.io/explicit_wait2.html'
         browser = webdriver.Chrome()
+        browser.get(url)
         main_functions()
     finally:
         time.sleep(7)
         browser.quit()
+
+
+
