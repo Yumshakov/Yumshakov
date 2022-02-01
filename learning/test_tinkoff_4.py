643f000a8e2a160d6be70bb5f7d0a0d29f572b80
Yumshakov <chyrilovo92@mail.ru>
2022-02-01 18:04:56 +0500

    Start learning Flask

--- learning/test_tinkoff_4.py
+++ learning/test_tinkoff_4.py
@@ -1,8 +1,21 @@
-input_data = list(map(int, input().split()))
-gurnal = input_data[1:]
-max = max(gurnal)
-min = min(gurnal)
-for index in range(len(gurnal)):
-    if gurnal[index] == max:
-        gurnal[index] = min
-print(*gurnal)
+phone_cod = {2:'abc',
+             3:'def',
+             4:'ghi',
+             5:'jkl',
+             6:'mno',
+             7:'pqrs',
+             8:'tuv',
+             9:'wxyz'}
+
+n = int(input())
+ministerstvo_list = [input() for i in range(n)]
+result_list = []
+result_string = ''
+for elem in ministerstvo_list:
+    for letter in elem:
+        for k, v in phone_cod.items():
+            if letter in v:
+                result_string += str(k)
+    result_list.append(result_string)
+    result_string = ''
+print(len(set(result_list)))
